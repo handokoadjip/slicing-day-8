691cc3422333a12a082e9d96c813a67c8ca9117e
handokoadjipangestu <handokoadjipangestu@gmail.com>
2022-02-16 16:54:31 +0700

    revision - added meta seo - added figure in img - change var to es5 - change document ready from vanila js to jquery - change alt - tidy up files

--- public_html/source/js/script.js
+++ public_html/source/js/script.js
@@ -1,19 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function () {
   function mobileViewUpdate() {
-    var viewportWidth = $(window).width();
+    const viewportWidth = $(window).width();
     if (viewportWidth < 600) {
-      $('.navbar.fixed-top').addClass('scrolled');
-      $('.navbar-brand').addClass('scrolled');
+      $(".navbar.fixed-top").addClass("scrolled");
+      $(".navbar-brand").addClass("scrolled");
     } else {
-      $(document).scroll(function() {
-        var $nav = $(".navbar.fixed-top");
-        var $brand = $(".navbar-brand");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-        $brand.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      $(document).scroll(function () {
+        const nav = $(".navbar.fixed-top");
+        const brand = $(".navbar-brand");
+        nav.toggleClass("scrolled", $(this).scrollTop() > nav.height());
+        brand.toggleClass("scrolled", $(this).scrollTop() > nav.height());
       });
     }
   }
-  
+
   $(window).on("load", mobileViewUpdate);
   $(window).on("resize", mobileViewUpdate);
 
